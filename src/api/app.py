1c9e63a3616d91cf39d65723b703cb3b81b5a8d5
Ankita-624 <ankitagouda624@gmail.com>
2025-08-30 04:09:43 +0530

    final update

--- src/api/app.py
+++ src/api/app.py
@@ -12,7 +12,7 @@ app = FastAPI(
 # --- CORS ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500","https://bank-churn-dashboard.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
